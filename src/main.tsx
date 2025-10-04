bfbdba18f18597d37caa2958b13df51e0455c5f7
EemI17 <59940214+EemI17@users.noreply.github.com>
2025-10-04 22:56:51 +0300

    Updated App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import ArkkitehtisuunnitteluPage from './ArkkitehtisuunnitteluPage.tsx';
 import RakennesuunnitteluPage from './RakennesuunnitteluPage.tsx';
+import RakennuttajapalvelutPage from './RakennuttajapalvelutPage.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/arkkitehtisuunnittelu" element={<ArkkitehtisuunnitteluPage />} />
         <Route path="/rakennesuunnittelu" element={<RakennesuunnitteluPage />} />
+        <Route path="/rakennuttajapalvelut" element={<RakennuttajapalvelutPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
