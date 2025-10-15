515d3839ab69095d8f57249519013e9eb08cd20a
EemI17 <59940214+EemI17@users.noreply.github.com>
2025-10-15 15:32:22 +0300

    Scroll to top on route change

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import App from './App.tsx';
 import ArkkitehtisuunnitteluPage from './ArkkitehtisuunnitteluPage.tsx';
 import RakennesuunnitteluPage from './RakennesuunnitteluPage.tsx';
@@ -10,9 +10,20 @@ import ProjektitPage from './ProjektitPage.tsx';
 import YhteystiedotPage from './YhteystiedotPage.tsx';
 import './index.css';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/arkkitehtisuunnittelu" element={<ArkkitehtisuunnitteluPage />} />
