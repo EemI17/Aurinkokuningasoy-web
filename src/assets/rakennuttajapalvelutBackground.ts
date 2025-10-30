fe2340f7212c47189790f24db8a68e5cd9b1d3d4
EemI17 <59940214+EemI17@users.noreply.github.com>
2025-10-30 21:42:11 +0200

    Inline rakennuttajapalvelut photo via base64 chunks

---
+++ src/assets/rakennuttajapalvelutBackground.ts
@@ -0,0 +1,42 @@
+// Auto-generated data URI for the Rakennuttajapalvelut service background photo.
+// The data is split into chunks to keep the source files reviewable and to avoid bundler limits.
+import chunk1 from './rakennuttajapalvelutBackground/chunk1';
+import chunk2 from './rakennuttajapalvelutBackground/chunk2';
+import chunk3 from './rakennuttajapalvelutBackground/chunk3';
+import chunk4 from './rakennuttajapalvelutBackground/chunk4';
+import chunk5 from './rakennuttajapalvelutBackground/chunk5';
+import chunk6 from './rakennuttajapalvelutBackground/chunk6';
+import chunk7 from './rakennuttajapalvelutBackground/chunk7';
+import chunk8 from './rakennuttajapalvelutBackground/chunk8';
+import chunk9 from './rakennuttajapalvelutBackground/chunk9';
+import chunk10 from './rakennuttajapalvelutBackground/chunk10';
+import chunk11 from './rakennuttajapalvelutBackground/chunk11';
+import chunk12 from './rakennuttajapalvelutBackground/chunk12';
+import chunk13 from './rakennuttajapalvelutBackground/chunk13';
+import chunk14 from './rakennuttajapalvelutBackground/chunk14';
+import chunk15 from './rakennuttajapalvelutBackground/chunk15';
+import chunk16 from './rakennuttajapalvelutBackground/chunk16';
+import chunk17 from './rakennuttajapalvelutBackground/chunk17';
+import chunk18 from './rakennuttajapalvelutBackground/chunk18';
+import chunk19 from './rakennuttajapalvelutBackground/chunk19';
+import chunk20 from './rakennuttajapalvelutBackground/chunk20';
+import chunk21 from './rakennuttajapalvelutBackground/chunk21';
+import chunk22 from './rakennuttajapalvelutBackground/chunk22';
+import chunk23 from './rakennuttajapalvelutBackground/chunk23';
+import chunk24 from './rakennuttajapalvelutBackground/chunk24';
+import chunk25 from './rakennuttajapalvelutBackground/chunk25';
+import chunk26 from './rakennuttajapalvelutBackground/chunk26';
+import chunk27 from './rakennuttajapalvelutBackground/chunk27';
+import chunk28 from './rakennuttajapalvelutBackground/chunk28';
+import chunk29 from './rakennuttajapalvelutBackground/chunk29';
+import chunk30 from './rakennuttajapalvelutBackground/chunk30';
+import chunk31 from './rakennuttajapalvelutBackground/chunk31';
+import chunk32 from './rakennuttajapalvelutBackground/chunk32';
+import chunk33 from './rakennuttajapalvelutBackground/chunk33';
+import chunk34 from './rakennuttajapalvelutBackground/chunk34';
+import chunk35 from './rakennuttajapalvelutBackground/chunk35';
+import chunk36 from './rakennuttajapalvelutBackground/chunk36';
+
+const rakennuttajapalvelutBackground = 'data:image/png;base64,' + chunk1 + chunk2 + chunk3 + chunk4 + chunk5 + chunk6 + chunk7 + chunk8 + chunk9 + chunk10 + chunk11 + chunk12 + chunk13 + chunk14 + chunk15 + chunk16 + chunk17 + chunk18 + chunk19 + chunk20 + chunk21 + chunk22 + chunk23 + chunk24 + chunk25 + chunk26 + chunk27 + chunk28 + chunk29 + chunk30 + chunk31 + chunk32 + chunk33 + chunk34 + chunk35 + chunk36;
+
+export default rakennuttajapalvelutBackground;
